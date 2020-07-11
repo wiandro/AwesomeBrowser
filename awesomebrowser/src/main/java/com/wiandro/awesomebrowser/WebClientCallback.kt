eff09fa238328357181ba637379e16d5dd2b54d9
Ehsan Abbasi <abbasi.ehsan1991@gmail.com>
2020-07-11 16:53:02 +0430

    Add new methods to WebClientCallback inorder to handle view actions

--- awesomebrowser/src/main/java/com/wiandro/awesomebrowser/WebClientCallback.kt
+++ awesomebrowser/src/main/java/com/wiandro/awesomebrowser/WebClientCallback.kt
@@ -1,14 +1,20 @@
 package com.wiandro.awesomebrowser
 
+import android.net.http.SslError
+import android.webkit.SslErrorHandler
+
 /**
  * CREATED BY Javadhme
  */
 interface WebClientCallback{
 
-    fun onLoadStart(url: String, sslError: Boolean)
+    fun onLoadStart(url: String)
 
-    fun onLoadFinish(url: String, sslError: Boolean)
+    fun onLoadFinish(url: String)
 
     fun needBackPress()
 
+    fun onErrorHappened(errorCode: Int, description: String?, failingUrl: String?)
+
+    fun onSslErrorHappened(handler: SslErrorHandler?, error: SslError?)
 }
